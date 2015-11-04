4ed93777e5f66d53ea763decc5e60e174a1f81fb
MarkDucommun <ducommun.mark@gmail.com>
2015-11-04 17:14:32 -0600

    checks that board initialization creates an array of arrays

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,16 @@ require_relative '../lib/board'
 require 'pry'
 
 describe Board do
+  let(:valid_board_string) { '1XXX2XXX3XXX4XXX' }
+
   it 'must be initialized with a string with N2xN2 length' do
     expect { Board.new('1XXX') }.to raise_error 'Invalid Board'
-    expect { Board.new('1XXX1XXX1XXX1XXX') }.to_not raise_error
+    expect { Board.new(valid_board_string) }.to_not raise_error
+  end
+
+  it 'splits the string into an array of arrays' do
+    board = Board.new(valid_board_string)
+    expect(board.array.length).to be 4
+    expect(board.array.first.length).to be 4
   end
 end
