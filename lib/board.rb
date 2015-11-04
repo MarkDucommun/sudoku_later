914aa4c36fb4e67248f32c17596d907d582f8332
MarkDucommun <ducommun.mark@gmail.com>
2015-11-04 17:01:30 -0600

    adds a private method that validates board string is an N2xN2 length

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,11 @@
 class Board
+  def initialize(board_string)
+    fail 'Invalid Board' unless valid_board_length?(board_string.length)
+  end
+
+  private
+
+  def valid_board_length?(board_length)
+    Math.sqrt(Math.sqrt(board_length)) % 1 == 0
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../lib/board'
+require 'pry'
 
 describe Board do
   it 'must be initialized with a string with N2xN2 length' do
-    expect { Board.new('1XXX') }.to raise_error InvalidBoardError
+    expect { Board.new('1XXX') }.to raise_error 'Invalid Board'
     expect { Board.new('1XXX1XXX1XXX1XXX') }.to_not raise_error
   end
 end
