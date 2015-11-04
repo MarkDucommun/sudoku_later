f603b4f42ad26c2fd3ff631a285c601c066a7c85
MarkDucommun <ducommun.mark@gmail.com>
2015-11-04 16:48:01 -0600

    first spec

---
+++ spec/board_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../board'
+
+describe Board do
+  it 'must be initialized with a string with N2xN2 length' do
+    expect { Board.new('1XXX') }.to raise_error InvalidBoardError
+    expect { Board.new('1XXX1XXX1XXX1XXX') }.to_not raise_error
+  end
+end
